cda61fa49f99549b0fbff23ed289825980d588f5
Abdul <uha24278@gmail.com>
2024-07-03 14:46:33 +0530

    third commit

--- src/test/java/com/tutorialsNinja/qa/testcases/LoginTest.java
+++ src/test/java/com/tutorialsNinja/qa/testcases/LoginTest.java
@@ -38,7 +38,9 @@ public class LoginTest {
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 			driver.get("https://tutorialsninja.com/demo/");
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 			driver.findElement(By.xpath("//span[text()='My Account']")).click();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 			driver.findElement(By.linkText("Login")).click();
 		
 	}
